c4d2a3c553c1a4f6fd016a5707bf5a969d113844
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2020-03-19 20:14:36 -0700

    New networking

--- root/startup.sh
+++ root/startup.sh
@@ -1,6 +1,5 @@
 #! /bin/sh
 
-HOME_INTERFACE=${__HOME_INTERFACE}
 DDNS_DOMAIN=minkebox.net
 PRIVATE_HOSTNAME=${__GLOBALID}
 DNSSERVER=${__DNSSERVER}
@@ -15,10 +14,10 @@ PUBLIC_KEY=${ROOT}/key.public
 PORT_FILE=${ROOT}/port
 PORTRANGE_START=41310
 PORTRANGE_LEN=256
-DEVICE=wg0
+DEVICE_INTERFACE=wg0
 
-HOME_CIDR=$(ip addr show dev ${HOME_INTERFACE} | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}/[0-9]{1,2}\b" | head -1)
-HOME_IP=$(echo ${HOME_CIDR} | sed "s/\/.*$//")
+DEFAULT_CIDR=$(ip addr show dev ${__DEFAULT_INTERFACE} | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}/[0-9]{1,2}\b" | head -1)
+DEFAULT_IP=$(echo ${DEFAULT_CIDR} | sed "s/\/.*$//")
 
 if [ "${SELECTED_SERVER_NETWORK}" != "" ]; then
   SERVER_NETWORK=${SELECTED_SERVER_NETWORK}
@@ -34,7 +33,7 @@ if [ ! -e ${PRIVATE_KEY} ]; then
     # Prime random
     RANDOM=$(head -1 /dev/urandom | cksum)
     # Select an unused port at random from within our standard range avoiding any we see as in use
-    active_ports=$(upnpc -m ${HOME_INTERFACE} -L | grep "^ *\d\? UDP\|TCP .*$" | sed "s/^.*:\(\d*\).*$/\1/")
+    active_ports=$(upnpc -m ${__NAT_INTERFACE} -L | grep "^ *\d\? UDP\|TCP .*$" | sed "s/^.*:\(\d*\).*$/\1/")
     while true ; do
       PORT=$((${PORTRANGE_START} + RANDOM % ${PORTRANGE_LEN}))
       if ! $(echo $active_ports | grep -q ${PORT}); then
@@ -62,11 +61,11 @@ DNS = ${DNSSERVER}
 [Peer]
 PublicKey = $(cat ${PUBLIC_KEY})
 Endpoint = ${PRIVATE_HOSTNAME}.${DDNS_DOMAIN}:${PORT}
-AllowedIPs = ${SERVER_CIDR}, ${HOME_CIDR}
+AllowedIPs = ${SERVER_CIDR}, ${DEFAULT_CIDR}
 __EOF__
 
 # Create server config
-cat > ${ROOT}/${DEVICE}.conf <<__EOF__
+cat > ${ROOT}/${DEVICE_INTERFACE}.conf <<__EOF__
 [Interface]
 PrivateKey = $(cat ${PRIVATE_KEY})
 Address = ${SERVER_CIDR}
@@ -76,7 +75,7 @@ __EOF__
 for client in $(cat ${CLIENTS_CONFIG}); do
   client_id=$(echo ${client} | sed "s/#.*//")
   client_key=$(echo ${client} | sed "s/.*#//")
-  cat >> ${ROOT}/${DEVICE}.conf <<__EOF__
+  cat >> ${ROOT}/${DEVICE_INTERFACE}.conf <<__EOF__
 [Peer]
 PublicKey = ${client_key}
 AllowedIPs = ${SERVER_NETWORK}.${client_id}/32
@@ -85,13 +84,13 @@ __EOF__
 done
 
 # Masquarade the vpn
-iptables -t nat -I POSTROUTING -o ${HOME_INTERFACE} -j MASQUERADE
+iptables -t nat -I POSTROUTING -o ${__DEFAULT_INTERFACE} -j MASQUERADE
 
 # Internet only
 if [ "${INTERNET_ONLY}" = "true" ]; then
-  iptables -A FORWARD -i ${DEVICE} -d 10.0.0.0/8 -j DROP
-  iptables -A FORWARD -i ${DEVICE} -d 172.16.0.0/12 -j DROP
-  iptables -A FORWARD -i ${DEVICE} -d 192.168.0.0/16 -j DROP
+  iptables -A FORWARD -i ${DEVICE_INTERFACE} -d 10.0.0.0/8 -j DROP
+  iptables -A FORWARD -i ${DEVICE_INTERFACE} -d 172.16.0.0/12 -j DROP
+  iptables -A FORWARD -i ${DEVICE_INTERFACE} -d 192.168.0.0/16 -j DROP
 fi
 
 # Start Wireguard
@@ -102,10 +101,10 @@ trap "killall sleep; exit" TERM INT
 # Open the NAT
 sleep 1 &
 while wait "$!"; do
-  upnpc -e ${HOSTNAME}_wg -a ${HOME_IP} ${PORT} ${PORT} ${PROTO} ${TTL}
+  upnpc -m ${__DEFAULT_INTERFACE} -e ${HOSTNAME}_wg -a ${DEFAULT_IP} ${PORT} ${PORT} ${PROTO} ${TTL}
   if [ "${__HOSTIP6}" != "" ]; then
-    upnpc -e ${HOSTNAME}_wg6 -6 -A "" 0 ${__HOSTIP6} ${PORT} ${PROTO} ${TTL}
+    upnpc -m ${__DEFAULT_INTERFACE}-e ${HOSTNAME}_wg6 -6 -A "" 0 ${__HOSTIP6} ${PORT} ${PROTO} ${TTL}
   fi
   sleep ${TTL2} &
 done
-upnpc -d ${PORT} ${PROTO}
+upnpc -m ${__DEFAULT_INTERFACE} -d ${PORT} ${PROTO}
